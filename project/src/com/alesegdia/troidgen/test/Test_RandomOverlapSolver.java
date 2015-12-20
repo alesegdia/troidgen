3dbd68725f807dcc042b6ca8f4564ee90c1f21b7
Alejandro Seguí <alesegdia@gmail.com>
2015-12-20 06:13:45 +0100

    tweaking random overlap test

--- project/src/com/alesegdia/troidgen/test/Test_RandomOverlapSolver.java
+++ project/src/com/alesegdia/troidgen/test/Test_RandomOverlapSolver.java
@@ -14,16 +14,19 @@ public class Test_RandomOverlapSolver {
 	{
 		RNG.rng = new RNG();
 		List<Rect> rects = new LinkedList<Rect>();
-		for( int i = 0; i < 50; i++ )
+		for( int i = 0; i < 20; i++ )
 		{
-			int x = RNG.rng.nextInt(-10, 10);
-			int y = RNG.rng.nextInt(-10, 10);
-			int s = RNG.rng.nextInt(1, 3);
-			rects.add(new Rect(x, y, s, s));
+			int x = RNG.rng.nextInt(-4, 4);
+			int y = RNG.rng.nextInt(-4, 4);
+			int w = RNG.rng.nextInt(1, 4);
+			int h = RNG.rng.nextInt(1, 4);
+			rects.add(new Rect(x, y, w, h));
 		}
 		
 		OverlapSolver os = new OverlapSolver();
 		OverlapSolverConfig osc = new OverlapSolverConfig();
+		osc.separationParameter = 1f;
+		osc.enableTweakNearSeparation = false;
 		
 		new RectDebugger(rects, 800, 600).Show();
 		rects = os.solve(rects, osc);
