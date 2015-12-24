8a232e449c41cdcc4e450d8a7eb6002e574ba89f
Alejandro Seguí <alesegdia@gmail.com>
2015-12-24 21:22:39 +0100

    adding IRoomGroupEvaluator interface

---
+++ project/src/com/alesegdia/troidgen/IRoomGroupEvaluator.java
@@ -0,0 +1,11 @@
+package com.alesegdia.troidgen;
+
+import java.util.List;
+
+import com.alesegdia.troidgen.util.Rect;
+
+public interface IRoomGroupEvaluator {
+
+	public boolean evaluate( List<Rect> group );
+	
+}
