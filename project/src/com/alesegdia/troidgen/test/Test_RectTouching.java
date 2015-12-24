7457d4ad609f932f4b0ca6bba5ef852e206078a5
Alejandro Seguí <alesegdia@gmail.com>
2015-12-24 19:05:53 +0100

    adding isTouching function to rect; adding test for it

---
+++ project/src/com/alesegdia/troidgen/test/Test_RectTouching.java
@@ -0,0 +1,23 @@
+package com.alesegdia.troidgen.test;
+
+import com.alesegdia.troidgen.util.Rect;
+
+public class Test_RectTouching {
+
+	public static void main( String[] args )
+	{
+		Rect rr = new Rect(0,0,3,3);
+		
+		Rect r1 = new Rect(-3,3,3,3);
+		Rect r2 = new Rect(3,3,3,3);
+		
+		Rect r3 = new Rect(-3,-3,3,3);
+		Rect r4 = new Rect(3,-3,3,3);
+
+		System.out.println("TL: " + rr.isTouching(r1));
+		System.out.println("BR: " + rr.isTouching(r2));
+		System.out.println("BL: " + rr.isTouching(r3));
+		System.out.println("TR: " + rr.isTouching(r4));
+	}
+	
+}
--- project/src/com/alesegdia/troidgen/util/Rect.java
+++ project/src/com/alesegdia/troidgen/util/Rect.java
@@ -1,5 +1,9 @@
 package com.alesegdia.troidgen.util;
 
+import com.alesegdia.troidgen.room.Direction;
+import com.alesegdia.troidgen.room.LinkPair;
+import com.alesegdia.troidgen.room.Room;
+
 public class Rect {
 	
 	public Vec2 position = new Vec2(0,0);
@@ -32,6 +36,32 @@ public class Rect {
 		bx2 = other.position.x + other.size.x;
 		by2 = other.position.y + other.size.y;
 		return ax1 < bx2 && ax2 > bx1 && ay1 < by2 && ay2 > by1;
+	}
+	
+	public boolean isTouching( Rect other )
+	{
+		int ax1, ay1, ax2, ay2;
+		ax1 = (int) this.position.x;
+		ay1 = (int) this.position.y;
+		ax2 = (int) (this.position.x + this.size.x);
+		ay2 = (int) (this.position.y + this.size.y);
+		
+		int bx1, by1, bx2, by2;
+		bx1 = (int) other.position.x;
+		by1 = (int) other.position.y;
+		bx2 = (int) (other.position.x + other.size.x);
+		by2 = (int) (other.position.y + other.size.y);
+		
+		boolean tl = ax1 == bx2 && ay1 == by2;
+		boolean br = ax2 == bx1 && ay2 == by1;
+
+		boolean bl = ax1 == bx2 && ay2 == by1;
+		boolean tr = ax2 == bx1 && ay1 == by2;
+		
+		return !( bx1 - ax2 > 0
+			   || ax1 - bx2 > 0
+			   || by1 - ay2 > 0
+			   || ay1 - by2 > 0 ) && !tl && !br && !bl && !tr;
 	}
 	
 }
