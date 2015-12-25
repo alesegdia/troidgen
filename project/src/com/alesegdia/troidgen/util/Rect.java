f5ed765453f5af3ab15ea5bb3b614d73a5fb2685
Alejandro Seguí <alesegdia@gmail.com>
2015-12-25 08:28:24 +0100

    using new x1, y1, x2, y2 methods

--- project/src/com/alesegdia/troidgen/util/Rect.java
+++ project/src/com/alesegdia/troidgen/util/Rect.java
@@ -1,9 +1,5 @@
 package com.alesegdia.troidgen.util;
 
-import com.alesegdia.troidgen.room.Direction;
-import com.alesegdia.troidgen.room.LinkPair;
-import com.alesegdia.troidgen.room.Room;
-
 public class Rect {
 	
 	public Vec2 position = new Vec2(0,0);
@@ -46,42 +42,21 @@ public class Rect {
 	
 	public boolean collideWith( Rect other )
 	{
-		float ax1, ay1, ax2, ay2, bx1, by1, bx2, by2;
-		ax1 = this.position.x;
-		ay1 = this.position.y;
-		ax2 = this.position.x + this.size.x;
-		ay2 = this.position.y + this.size.y;
-		bx1 = other.position.x;
-		by1 = other.position.y;
-		bx2 = other.position.x + other.size.x;
-		by2 = other.position.y + other.size.y;
-		return ax1 < bx2 && ax2 > bx1 && ay1 < by2 && ay2 > by1;
+		return x1() < other.x2() && x2() > other.x1() && y1() < other.y2() && y2() > other.y1();
 	}
 	
 	public boolean isTouching( Rect other )
 	{
-		int ax1, ay1, ax2, ay2;
-		ax1 = (int) this.position.x;
-		ay1 = (int) this.position.y;
-		ax2 = (int) (this.position.x + this.size.x);
-		ay2 = (int) (this.position.y + this.size.y);
-		
-		int bx1, by1, bx2, by2;
-		bx1 = (int) other.position.x;
-		by1 = (int) other.position.y;
-		bx2 = (int) (other.position.x + other.size.x);
-		by2 = (int) (other.position.y + other.size.y);
-		
-		boolean tl = ax1 == bx2 && ay1 == by2;
-		boolean br = ax2 == bx1 && ay2 == by1;
+		boolean tl = x1() == other.x2() && y1() == other.y2();
+		boolean br = x2() == other.x1() && y2() == other.y1();
 
-		boolean bl = ax1 == bx2 && ay2 == by1;
-		boolean tr = ax2 == bx1 && ay1 == by2;
+		boolean bl = x1() == other.x2() && y2() == other.y1();
+		boolean tr = x2() == other.x1() && y1() == other.y2();
 		
-		return !( bx1 - ax2 > 0
-			   || ax1 - bx2 > 0
-			   || by1 - ay2 > 0
-			   || ay1 - by2 > 0 ) && !tl && !br && !bl && !tr;
+		return !( other.x1() - x2() > 0
+			   || x1() - other.x2() > 0
+			   || other.y1() - y2() > 0
+			   || y1() - other.y2() > 0 ) && !tl && !br && !bl && !tr;
 	}
 	
 }
