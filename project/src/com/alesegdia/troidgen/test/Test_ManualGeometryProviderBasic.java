493c6580639ea250d3701122bcaf93596b422f4c
Alejandro Seguí <alesegdia@gmail.com>
2015-12-29 15:27:44 +0100

    updating manual geometry provider test

--- project/src/com/alesegdia/troidgen/test/Test_ManualGeometryProviderBasic.java
+++ project/src/com/alesegdia/troidgen/test/Test_ManualGeometryProviderBasic.java
@@ -1,6 +1,9 @@
 package com.alesegdia.troidgen.test;
 
+import java.util.List;
+
 import com.alesegdia.troidgen.ManualRoomProvider;
+import com.alesegdia.troidgen.restriction.RestrictionSet;
 import com.alesegdia.troidgen.room.Room;
 import com.alesegdia.troidgen.util.RNG;
 
@@ -12,18 +15,31 @@ public class Test_ManualGeometryProviderBasic {
 
 		ManualRoomProvider mgp = new ManualRoomProvider();
 		
-		mgp.addGeometryModel(new Room(3, 3, null));
-		mgp.addGeometryModel(new Room(1, 2, null));
-		mgp.addGeometryModel(new Room(2, 1, null));
-		mgp.addGeometryModel(new Room(1, 3, null));
-		mgp.addGeometryModel(new Room(3, 3, null));
-		mgp.addGeometryModel(new Room(1, 3, null));
-		mgp.addGeometryModel(new Room(2, 1, null));
-		mgp.addGeometryModel(new Room(3, 3, null));
-		mgp.addGeometryModel(new Room(3, 3, null));
+		mgp.addGeometryModel(new Room(3, 3, new RestrictionSet(4, true, false, false, false)));
+		mgp.addGeometryModel(new Room(1, 2, new RestrictionSet(4, true, true, false, false)));
+		mgp.addGeometryModel(new Room(2, 1, new RestrictionSet(4, true, true, false, true)));
+		mgp.addGeometryModel(new Room(1, 3, new RestrictionSet(4, true, true, false, true)));
+		mgp.addGeometryModel(new Room(3, 3, new RestrictionSet(4, true, false, true, false)));
+		mgp.addGeometryModel(new Room(1, 3, new RestrictionSet(4, true, false, true, false)));
+		mgp.addGeometryModel(new Room(2, 1, new RestrictionSet(4, true, false, false, false)));
+		mgp.addGeometryModel(new Room(3, 3, new RestrictionSet(4, true, false, false, true)));
+		mgp.addGeometryModel(new Room(3, 3, new RestrictionSet(4, true, false, false, false)));
+		mgp.addGeometryModel(new Room(3, 3, new RestrictionSet(4, true, false, false, false)));
 		
 		mgp.debug();
-
+		
+		test(mgp, 10, new RestrictionSet(4, true, false, false, false));
+		// test(mgp, 10, new RestrictionSet(4, false, false, true, false)); fails because no elements resolves
+		test(mgp, 10, new RestrictionSet(4, true, false, false, false));
+		test(mgp, 10, new RestrictionSet(4, true, false, true, false));
+		test(mgp, 10, new RestrictionSet(4, true, false, false, false));
+	}
+	
+	private static void test( ManualRoomProvider mgp, int numRooms, RestrictionSet rs )
+	{
+		System.out.println("");
+		List<Room> l = mgp.provideRandomList(numRooms, rs);
+		System.out.println(l);
 	}
 
 }
