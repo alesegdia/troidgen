4093ccd29246f930a514671caded441f9a6ae055
Alejandro Seguí <alesegdia@gmail.com>
2015-12-29 15:25:08 +0100

    adding static function to get a direction from a char at Direction

--- project/src/com/alesegdia/troidgen/room/Direction.java
+++ project/src/com/alesegdia/troidgen/room/Direction.java
@@ -1,5 +1,16 @@
 package com.alesegdia.troidgen.room;
 
 public enum Direction {
-	TOP, DOWN, LEFT, RIGHT, NODIR
+	TOP, DOWN, LEFT, RIGHT, NODIR;
+
+	public static Direction getFromChar(char charAt) {
+		switch(charAt)
+		{
+		case 'T': return TOP;
+		case 'B': return DOWN;
+		case 'L': return LEFT;
+		case 'R': return RIGHT;
+		}
+		return NODIR;
+	}
 }
