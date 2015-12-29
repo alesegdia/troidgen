d457ea99d233ba6e40d4022b1c291797da8b6b1b
Alejandro Seguí <alesegdia@gmail.com>
2015-12-29 15:25:28 +0100

    renaming position at LinkInfo

--- project/src/com/alesegdia/troidgen/room/LinkInfo.java
+++ project/src/com/alesegdia/troidgen/room/LinkInfo.java
@@ -4,8 +4,7 @@ import com.alesegdia.troidgen.util.Vec2;
 
 public class LinkInfo {
 
-	public Vec2 coordinate = new Vec2(0,0);
+	public Vec2 relativeCoordinate = new Vec2(0,0);
 	public Direction direction = Direction.NODIR;
-	public Vec2 position = new Vec2(0,0);
 	
 }
