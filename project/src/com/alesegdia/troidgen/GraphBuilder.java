6b97b1866c5391aa687c1aedd407cf03df5b39c9
Alejandro Seguí <alesegdia@gmail.com>
2015-12-31 22:19:20 +0100

    returning graph matrix from graph builder

--- project/src/com/alesegdia/troidgen/GraphBuilder.java
+++ project/src/com/alesegdia/troidgen/GraphBuilder.java
@@ -3,11 +3,13 @@ package com.alesegdia.troidgen;
 import java.util.List;
 
 import com.alesegdia.troidgen.room.Room;
+import com.alesegdia.troidgen.util.UpperMatrix2D;
 
 public class GraphBuilder {
 
-	public void generate( List<Room> rects )
+	public UpperMatrix2D<Float> build( List<Room> rects )
 	{
+		UpperMatrix2D<Float> m = new UpperMatrix2D<Float>( rects.size(), rects.size(), Float.MAX_VALUE );
 		for( Room router : rects )
 		{
 			for( Room rinner : rects )
@@ -15,9 +17,11 @@ public class GraphBuilder {
 				if( router != rinner && router.isTouching(rinner) )
 				{
 					router.neighboors.add(rinner);
+					m.Set(rects.indexOf(router), rects.indexOf(rinner), router.position.distance(rinner.position));
 				}
 			}
 		}
+		return m;
 	}
 	
 }
