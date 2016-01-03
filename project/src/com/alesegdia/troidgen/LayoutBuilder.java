7f7e476807179ea60981dc2d36d73bdf0a0d53f6
Alejandro Seguí <alesegdia@gmail.com>
2016-01-03 21:13:17 +0100

    adding pre-fixed room list parameter to layout builder

--- project/src/com/alesegdia/troidgen/LayoutBuilder.java
+++ project/src/com/alesegdia/troidgen/LayoutBuilder.java
@@ -9,29 +9,27 @@ import com.alesegdia.troidgen.room.Room;
 import com.alesegdia.troidgen.util.MiscRoomUtils;
 
 public class LayoutBuilder {
+	public List<Room> generate( LayoutBuilderConfig lbc, IRoomProvider geometryProvider, IRoomGroupValidator rge, RestrictionSet rs ) {
+		return generate(lbc, geometryProvider, rge, rs, new LinkedList<Room>());
+	}
 
-	public List<Room> generate( LayoutBuilderConfig lbc, IRoomProvider geometryProvider, IRoomGroupValidator rge, RestrictionSet rs )
+	public List<Room> generate( LayoutBuilderConfig lbc, IRoomProvider geometryProvider, IRoomGroupValidator rge, RestrictionSet rs, List<Room> fixedGroup )
 	{
 		RNG.rng = new RNG();
 		OverlapSolver os = new OverlapSolver();
 
-		List<Room> outputLayout = new LinkedList<Room>();
+		List<Room> outputLayout = fixedGroup;
 
 		while( !rge.validate(outputLayout) )
 		{
-			//System.out.println(i);
 			List<Room> testRects = geometryProvider.provideRandomList( lbc.numRooms, rs );
 			MiscRoomUtils.RandomPlaceInRange(testRects, lbc.spawnRect);
 			testRects.addAll(outputLayout);
 			testRects = os.solve(lbc.osc, testRects, outputLayout);
 
-
 			GroupExtractor ge = new GroupExtractor();
 			List<List<Room>> groups = ge.solve(testRects);
 			outputLayout = getBestGroup(groups);
-			
-			//new RectDebugger(outputLayout, 800, 600).Show();
-
 		}
 
 		return outputLayout;
