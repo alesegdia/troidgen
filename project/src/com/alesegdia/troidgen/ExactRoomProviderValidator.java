2c59462cc6d1d255db4168861b9e267f0088442a
Alejandro Seguí <alesegdia@gmail.com>
2016-01-09 18:28:02 +0100

    updating exact room provider validator to use an exact room provider instead of a generic IRoomProvider

--- project/src/com/alesegdia/troidgen/ExactRoomProviderValidator.java
+++ project/src/com/alesegdia/troidgen/ExactRoomProviderValidator.java
@@ -3,19 +3,21 @@ package com.alesegdia.troidgen;
 import java.util.List;
 
 import com.alesegdia.troidgen.room.Room;
+import com.alesegdia.troidgen.util.Logger;
 
 public class ExactRoomProviderValidator implements IRoomGroupValidator {
 
-	private IRoomProvider rp;
+	private ExactRoomProvider rp;
 
-	public ExactRoomProviderValidator( IRoomProvider rp )
+	public ExactRoomProviderValidator( ExactRoomProvider erp )
 	{
-		this.rp = rp;
+		this.rp = erp;
 	}
 	
 	@Override
 	public boolean validate(List<Room> group) {
-		return this.rp.isOk();
+		Logger.Log(group.size());
+		return this.rp.isOk(); // || group.size() > this.rp.totalNeeded() / 4;
 	}
 
 }
