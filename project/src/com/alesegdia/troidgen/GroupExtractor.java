4d7aaed506aacc39b84adc07c93fc85641dcb26e
Alejandro Seguí <alesegdia@gmail.com>
2016-01-17 04:56:15 +0100

    fixing rgv usage at group extractor

--- project/src/com/alesegdia/troidgen/GroupExtractor.java
+++ project/src/com/alesegdia/troidgen/GroupExtractor.java
@@ -25,15 +25,23 @@ public class GroupExtractor {
 				// if this room was not assigned to any group
 				if( !assignedRooms.contains(outer_room) )
 				{
-					// we create a group because we will be collecting all connected rooms from this one
-					List<Room> currentGroup = new LinkedList<Room>();
-					currentGroup.add(outer_room);
-					assignedRooms.add(outer_room);
+					if( rgv == null || rgv.validate(outer_room) )
+					{
+						// we create a group because we will be collecting all connected rooms from this one
+						List<Room> currentGroup = new LinkedList<Room>();
+						currentGroup.add(outer_room);
+						assignedRooms.add(outer_room);
+						
+						collectConnected( testRects, assignedRooms, currentGroup, outer_room, rgv );
+						
+						// add the group to the return list
+						rectGroups.add(currentGroup);
+					}
 					
-					collectConnected( testRects, assignedRooms, currentGroup, outer_room, rgv );
-					
-					// add the group to the return list
-					rectGroups.add(currentGroup);
+					if( rgv != null && !rgv.validate(outer_room) && !assignedRooms.contains(outer_room) )
+					{
+						assignedRooms.add(outer_room);
+					}
 				}
 			}
 		}
@@ -46,7 +54,7 @@ public class GroupExtractor {
 		// iterate over all rooms to check which ones are connected
 		for( Room inner_room : testRects )
 		{
-			if( rgv == null || rgv.validate(r) )
+			if( rgv == null || rgv.validate(inner_room) )
 			{
 				// if it's not the same room, it is not assigned and it is touching the other room
 				if( r != inner_room && !assignedRooms.contains(inner_room) && r.isTouching(inner_room) && r.getPossibleConnections(inner_room).size() > 0 && !inner_room.collideWith(r) )
@@ -59,6 +67,10 @@ public class GroupExtractor {
 				
 					collectConnected( testRects, assignedRooms, currentGroup, inner_room, rgv );
 				}
+			}
+			if( rgv != null && !rgv.validate(inner_room) && !assignedRooms.contains(inner_room) )
+			{
+				assignedRooms.add(inner_room);
 			}
 		}
 	}
